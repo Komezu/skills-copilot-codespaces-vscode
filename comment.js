1ef946c0c66f7d501a44cffc23a90894e31c0aab
Jenny Qi <jin.qi@intrado.com>
2024-04-15 20:59:17 +0000

    Copilot third commit

---
+++ comment.js
@@ -0,0 +1,25 @@
+// Create web server
+const express = require('express');
+const app = express();
+
+// Serve static files
+app.use(express.static('public'));
+
+// Get comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+// Listen to port 3000
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+// Comments data
+const comments = [
+  { id: 1, author: 'John', body: 'Cool!' },
+  { id: 2, author: 'Jane', body: 'Nice!' },
+  { id: 3, author: 'Jack', body: 'Great!' },
+];
+
+module.exports = comments;
